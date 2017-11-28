7d8454ce62f763b5af3d710395103422fbe1d248
sikychen <chensiqi247@gmail.com>
2017-11-29 00:07:00 +0800

    2017/11/29

--- day-2/homework/index.js
+++ day-2/homework/index.js
@@ -6,7 +6,13 @@ const http = require('http')
 ,sillyDatetime = require('silly-datetime')
 ,path = require('path')
 
+let mime = null
+
 let server = http.createServer((req, res) => {
+  if(req.url == '/favicon.ico') {
+    res.end("")
+    return
+  }
   // 进入 管理后台
   if(req.url == '/admin') {
     fs.readFile(__dirname + '/views/admin.ejs', (err, data) => {
@@ -65,6 +71,7 @@ let server = http.createServer((req, res) => {
   let tTemp = '', num = 0
   if(req.url == '/admin/uploadPhoto' && req.method.toLowerCase() == 'post') {
     var form = new formidable.IncomingForm();
+    form.uploadDir = __dirname + "/uploads"
     form.parse(req, function(err, fields, files) {
       if(err) {
         throw Error('上传图片失败！')
@@ -82,6 +89,7 @@ let server = http.createServer((req, res) => {
       }
       let ran = ('0000' + num).substr(-5)
       // 后缀名
+      console.log(files.tupian.path)
       let extname = path.extname(files.tupian.name)
       let newpath = `uploads/${fields.album}/${t}${ran}${extname}`
       fs.rename(files.tupian.path, newpath, err => {
@@ -129,20 +137,22 @@ let server = http.createServer((req, res) => {
       if(err) {
         throw Error('读取 uploads 文件夹失败！')
       }
-      let isDir = false
+      console.log(req.url.substr(1))
+      let isDir = false, dirName = ''
       for(var i=0; i<files.length; i++) {
-        if(files[i] == req.url.substr(1)) {
+        if(files[i] == req.url.substr(8)) {
           isDir = true
+          dirName = files[i]
         }
       }
       if(isDir) {
-        fs.readdir(__dirname + '/uploads' + req.url, (err, files) => {
+        fs.readdir(__dirname + '/uploads/' + dirName, (err, files) => {
           if(err) {
-            throw Error('读取 '+req.url+' 文件夹失败！')
+            throw Error('读取 '+dirName+' 文件夹失败！')
           }
-          let d = {name: req.url.substr(1),photos: []}
+          let d = {name: dirName,photos: []}
           for(var i=0; i<files.length; i++) {
-            d.photos.push({name:files[i], src: '/uploads/' + files[i]})
+            d.photos.push({name:files[i], src: `/images/${dirName}/${files[i]}`})
           }
           fs.readFile(__dirname + '/views/album.ejs', (err, data) => {
             let template = data.toString()
@@ -159,8 +169,41 @@ let server = http.createServer((req, res) => {
     return
   }
 
+  // 请求图片
+  if(req.url.substr(0,8) == '/images/') {
+    // 读对应图片
+    let imgName = req.url.substr(8)
+    fs.readFile(__dirname + '/uploads/' + imgName, (err, data) => {
+      if(err) {
+        console.log('读取 imgName 图片失败！')
+        res.writeHead(404, {'content-type': 'text/html; charset=utf-8'})
+        res.end('<h1>404</h1><p>找不到该页面</p>')
+      }else {
+        let extname = path.extname(req.url)
+        getMime(extname, mime => {
+          res.writeHead(200, {'content-type': mime})
+          res.end(data)
+        })
+      }
+    })
+    return
+  }
+
 })
 
 server.listen(8080)
 
 console.log('server created...')
+
+
+function getMime(extname, cb) {
+  if(mime) {
+    cb(mime[extname] || 'text/plain')
+    return
+  }
+  fs.readFile('./mime.json', (err, data) => {
+    if(err) throw Error('fail to read mime.json.')
+    mime = JSON.parse(data)
+    cb(mime[extname] || 'text/plain')
+  })
+}
