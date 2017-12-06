1b6060b8aee5d3f4366fbe8adf5b77b17af7fdc0
siky <chensiqi247@gmail.com>
2017-12-06 16:19:58 +0800

    2017/12/06

--- part5/02.js
+++ part5/02.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 ,db = require('./model/db.js')
 
+// 增
 app.get('/api/insert', (req, res) => {
   let data = {
     name: ['小红', '小黄', '小杨'][parseInt(Math.random()*3)]
@@ -14,8 +15,65 @@ app.get('/api/insert', (req, res) => {
       console.log('插入数据失败')
       return
     }
+    console.log('插入数据成功')
     res.send('插入数据成功')
   })
+})
+
+// 查
+app.get('/api/find', (req, res) => {
+  let page = req.query.page || 0
+  ,pageSize = req.query.pageSize || 0
+
+  db.find('user1', {}, { page: page, pageSize: pageSize }, (err, result) => {
+    if(err) {
+      console.log(err)
+      console.log('查找数据失败')
+      return
+    }
+    res.json({result : result})
+    console.log('查找数据成功')
+  })
+})
+
+// 删
+app.get('/api/delete', (req, res) => {
+  let target = {}
+  if(req.query.name) {
+    target.name = req.query.name
+  }
+  if(req.query.age) {
+    target.age = {$gt:parseInt(req.query.age)}
+  }
+  db.deleteMany('user1', target, (err, result) => {
+    if(err) {
+      console.log(err)
+      console.log('删除数据失败')
+      return
+    }
+    res.send(result)
+  })
+})
+
+// 改
+app.get('/api/update', (req, res) => {
+  db.updateMany(
+    'user1'
+    ,{
+      name: '小黄'
+    }
+    ,{
+      $set: {name: '小橘黄'}
+    }
+    ,(err, result) => {
+      if(err) {
+        console.log(err)
+        console.log('修改数据失败')
+        return
+      }
+      res.send(result)
+    }
+  )
 })
 
 app.listen(8080)
--- part5/model/db.js
+++ part5/model/db.js
@@ -12,14 +12,109 @@ function __connetDB(callback) {
 /**
  * 增
  * @param collectionName string 要插入的集合
- * @param data JSON/Object 要插入的数据
- * @param callback Function 回调
+ * @param target Object 要插入的数据
+ * @param callback(err, result) Function 回调
+ * callback 返回两个参数
+ * @param err 错误对象，若为null，则无错误
+ * @param result 查找结果，若为null，则查找失败
  */
-exports.insertOne = (collectionName, data, callback) => {
+exports.insertOne = (collectionName, target, callback) => {
   __connetDB((err, db) => {
     if(err) {callback(err, null); return}
-    db.collection(collectionName).insertOne(data, (err, result) => {
+    db.collection(collectionName).insertOne(target, (err, result) => {
       callback(err, result)
+      db.close()
+    })
+  })
+}
+
+/**
+ * 查
+ * @param collectionName string 要插入的集合
+ * @param target Object 要插入的数据
+ * @param rule Object 查找规则
+ *        @param pageSize num 分页选项，单页数目
+ *        @param page num 分页选项，页数
+ *        @param sort Obj 排序方式
+ * @param callback(err, result) Function 回调
+ * callback 返回两个参数
+ * @param err 错误对象，若为null，则无错误
+ * @param result Arr 查找结果，若为null，则查找失败
+ */
+exports.find = (collectionName, target, rule, callback) => {
+  if(!target) {
+    console.log('db.find 参数错误')
+    return
+  }
+  // 分页限制初始化
+  let pageSize = parseInt(rule.pageSize || 0)
+  ,page = parseInt(rule.page || 0) > 0 ? parseInt(rule.page) - 1 : 0
+  ,sort = rule.sort || {}
+  ,result = []
+  __connetDB((err, db) => {
+    if(err) {callback(err, null); return}
+    // 查找目标
+    let cursor = db.collection(collectionName).find(target)
+      // 按分页查找
+      .skip(page*pageSize).limit(pageSize)
+      // 排序
+      .sort(sort)
+    // 查找完毕，放入 arr 数组
+    cursor.each((err, doc) => {
+      if(err) {
+        callback(err, null)
+        db.close()
+        return
+      }
+      if(doc != null) {
+        result.push(doc)
+      }else {
+        // 遍历结束
+        callback(null, result)
+        db.close()
+      }
+    })
+  })
+}
+
+/**
+ * 删
+ */
+exports.deleteMany = (collectionName, target, callback) => {
+  __connetDB((err, db) => {
+    if(err) {callback(err, null); return}
+    db.collection(collectionName).deleteMany(target, (err, result) => {
+      callback(err, result)
+      db.close()
+    })
+  })
+}
+
+/**
+ * 改
+ */
+exports.updateMany = (collectionName, target, target2, callback) => {
+  __connetDB((err, db) => {
+    if(err) {callback(err, null); return}
+    db.collection(collectionName).updateMany(
+      target, 
+      target2, 
+      (err, result) => {
+        callback(err, result)
+        db.close()
+      }
+    )
+  })
+}
+
+/**
+ * 获取总数目
+ */
+exports.getCount = (collectionName, callback) => {
+  __connetDB((err, db) => {
+    if(err) {callback(err, null); return}
+    db.collection(collectionName).count({}).then(count => {
+      callback(null, count)
       db.close()
     })
   })
